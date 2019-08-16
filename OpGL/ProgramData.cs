538566a33ae906f44e3b9621625521beec196341
DaKook <dakook123@gmail.com>
2019-08-16 11:10:33 -0500

    Added ProgramData.

---
+++ OpGL/ProgramData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenGL;
+
+namespace OpGL
+{
+    public class ProgramData
+    {
+        public uint ID;
+        public int ModelLocation;
+        public int TexLocation;
+        public int ColorLocation;
+        public int MasterColorLocation;
+
+        public ProgramData(uint id)
+        {
+            ID = id;
+            ModelLocation = Gl.GetUniformLocation(id, "model");
+            TexLocation = Gl.GetUniformLocation(id, "texMatrix");
+            ColorLocation = Gl.GetUniformLocation(id, "color");
+            MasterColorLocation = Gl.GetUniformLocation(id, "masterColor");
+        }
+    }
+}
